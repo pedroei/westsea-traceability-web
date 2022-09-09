b9f7040de78546fc046705904e3536d6ae065ffd
claudiaribeiro <claudia.ribeiro@atlanse.pt>
2022-09-09 15:23:46 +0100

    change design of productTraceability.component.html page

--- src/app/pages/productTraceability/productTraceability.component.ts
+++ src/app/pages/productTraceability/productTraceability.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { TraceabilityService } from 'src/app/Services/traceability.service';
-import { Produto } from 'src/app/types/produto';
-import { Edge, Node, ClusterNode, Layout } from '@swimlane/ngx-graph';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { UserService } from 'src/app/Services/user.service';
-import { Subject } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {TraceabilityService} from 'src/app/Services/traceability.service';
+import {Produto} from 'src/app/types/produto';
+import {Edge, Node} from '@swimlane/ngx-graph';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-productTraceability',
@@ -16,11 +14,7 @@ export class ProductTraceabilityComponent implements OnInit {
   loading = true;
   traceability = false;
 
-  showNav = true;
-
-  produto!: Produto;
-
-  nodeCheck: string[] = [];
+  product!: Produto;
 
   links: Edge[] = [];
 
@@ -33,8 +27,7 @@ export class ProductTraceabilityComponent implements OnInit {
   constructor(
     private router: Router,
     private traceabilityService: TraceabilityService,
-    private helper: JwtHelperService,
-    private userService: UserService
+    private readonly translate: TranslateService
   ) {
     this.router.routeReuseStrategy.shouldReuseRoute = function () {
       return false;
@@ -47,23 +40,13 @@ export class ProductTraceabilityComponent implements OnInit {
   }
 
   ngOnInit() {
-    const token = this.helper.decodeToken(this.userService.getUserToken);
-    if (
-      token.roles.includes('ROLE_CLIENT') &&
-      !token.roles.includes('ROLE_EMPLOYEE') &&
-      !token.roles.includes('ROLE_ADMIN')
-    ) {
-      this.showNav = false;
-    }
     this.load(this.router.url.split('/')[2]);
   }
 
   load(referenceNumber: string) {
     this.traceabilityService.getTrace(referenceNumber).subscribe((data) => {
-      this.produto = data;
-      console.log('teste');
-      console.log(this.produto);
-      if (this.produto.activity) {
+      this.product = data;
+      if (this.product.activity) {
         this.createGraph();
         this.traceability = true;
       }
@@ -77,17 +60,16 @@ export class ProductTraceabilityComponent implements OnInit {
     this.nodes.push({
       id: id,
       label: [
-        'Quantidade Disponivel',
-        this.produto.availableQuantity.toString(),
+        this.translate.instant("PRODUCTS.AVAILABLE_QUANTITY"),
+        this.product.availableQuantity.toString(),
       ].join(' : '),
       data: {
-        reference: this.produto.referenceNumber,
-        designation: this.produto.designation,
+        reference: this.product.referenceNumber,
+        designation: this.product.designation,
       },
     });
-    console.log('NODE ADDED');
-    if (this.produto.activity) {
-      for (let i of this.produto.activity.inputProductLots) {
+    if (this.product.activity) {
+      for (let i of this.product.activity.inputProductLots) {
         this.createNode(id, i);
       }
     }
@@ -99,7 +81,7 @@ export class ProductTraceabilityComponent implements OnInit {
     this.nodes.push({
       id: id,
       label: [
-        'Quantidade Utilizada',
+        this.translate.instant("PRODUCTS.QUANTITY_USED"),
         product.usedQuantityAsInput.toString(),
       ].join(' : '),
       data: {
@@ -107,14 +89,12 @@ export class ProductTraceabilityComponent implements OnInit {
         designation: product.designation,
       },
     });
-    console.log('NODE ADDED');
 
     this.links.push({
       id: this.makeid(16),
       source: parent,
       target: id,
     });
-    console.log('Link ADDED');
     if (product.activity) {
       for (let i of product.activity.inputProductLots) {
         this.createNode(id, i);
@@ -135,11 +115,5 @@ export class ProductTraceabilityComponent implements OnInit {
 
   goTo(id: string) {
     this.router.navigate([`/produtos/${id}`]);
-  }
-
-  sair() {
-    console.log('sair');
-    localStorage.clear();
-    this.router.navigate(['/login']);
   }
 }
